497fb7816e27fc3bd9bf620f75d7351cd5e38eeb
pedrob14 <pedrob14farias@gmail.com>
2023-01-22 10:33:10 -0300

    Selecionando, guardando, extraindo e mostrando em tela os dados
    Uso do querySelector() para selecionar o conteúdo.
    Uso do addEventListener() para "ouvir" uma atitvidade e realizar uma ação através de função.
    Uso do toLocaleDateString() para trocar uma linguagem.
    Uso do slice() para realizar recortes de uma string.
    Uso do localStorage para guardar na memória informações para serem utilizados depois.
    Uso do setItem() para guardar novas informações.
    Uso do getItem() para pegar as informações.
    Uso do JSON.stringify() para tranformá-lo em string.
    Uso do JSON.parse() para tranformá-lo em objeto.
    Uso do setData para extrair as informações.
    E o uso do load() para carregar todas as informações e mostrar em tela.

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
-const form = document.querySelector('#form-habits') /*Variável form recebendo o conteúdo do #form-habits*/
-const nlwSetup = new NLWSetup(form) /*Criando um novo objeto NLWSetup e atribuindo a variável nlwSetup*/
+const form =
+  document.querySelector(
+    "#form-habits"
+  ) /*Variável form recebendo o conteúdo do #form-habits*/
+const nlwSetup = new NLWSetup(
+  form
+) /*Criando um novo objeto NLWSetup e atribuindo a variável nlwSetup*/
+const button = document.querySelector("header button") // Selecionando a tag header e depois button
 
-const data = { /*Colocando dados dentro da variavel data*/
-  /*Array é uma estrutura de dados que faz uso de colchetes*/
-  run: ["01-01", "01-02", "01-06"],
-  water: ["01-04", "01-05"],
-  food: ["01-01", "01-03"],
+button.addEventListener("click", add) //addEventListener() é um métódo/evento que adiciona algo que vai ouvir no evento. Neste caso, sempre que clicar no botão ele vai ouvir o evento de 'click' e realizar uma ação, que no caso, é adicionar na função add().
+form.addEventListener("change", save)
+//Neste caso, sempre que selecionar no formulário ele vai ouvir o evento de 'change' e realizar uma ação, que no caso, é salvar os dados na função save().
+function add() {
+  //Ao clicar no botão, a função será executada
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  // new Date() vai criar uma nova data.
+  //toLocaleDateString() é uma função para trocar a linguagem da data, no caso inserido como parâmetro "pt-br".
+  //slice() Utilizado somente em objetos do tipo string. Slice significa recorte/fatiar. Nesse caso, o primeiro argumento, 0, não vai recortar nada do início da string. Enquanto o segundo argumento, 5, recorta do final da string.
+  const dayExists = nlwSetup.dayExists(today)
+  if (dayExists) {
+    alert("Dia já incluso 🔴")
+    return //return para o programa. Não vai para a próxima linha
+  }
+  alert("Adicionado com sucesso ✅")
+  nlwSetup.addDay(today)
 }
 
-nlwSetup.setData(data)/*Puxando as informações de dentro de data com setData e atribundo ao objeto nlwSetup*/
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+  //localStorage é um objeto que guarda na memória informações/dados para poder ser utilizado depois
+  // Para guardar a infomação é preciso usar um método chamado setItem(). Uso de 2 parâmetros.
+  // O 1º para colocar uma chave. No caso, NLWSetup@habits é a chave criada para pegar os dados depois. Poderia ser qualquer nome.
+  // O 2º para inserir uma string. Uso do JSON.stringify para tranformá-lo em string.
+}
+// const data = {
+//   /*Colocando dados dentro da variavel data*/
+//   /*Array é uma estrutura de dados que faz uso de colchetes*/
+// Aqui estava selecionando manualmente, mas deverá ser por meio de função
+//   run: ["01-01", "01-02", "01-06", "01-07", "01-08"],
+//   takePills: ["01-03"],
+//   journal: ["01-02"],
+// }
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+//Pegou do localStorage as informações que estavam em texto
+// transformou a string em objeto com JSON através do método parse().
+// getItem é um método para pegar as informações que está guardada em algum lugar
+// Atribuiu a variavel data.
+// Uso do || {} para não dá erro informando que está vazio.
+// Então está informando que irá pegar um novo dado ou estará vazio
+
+nlwSetup.setData(
+  data
+) /*Extrai as informações de dentro de data com setData e atribundo ao objeto nlwSetup*/
 nlwSetup.load() /*Ele carregará os dados internos e renderizará o layout*/
